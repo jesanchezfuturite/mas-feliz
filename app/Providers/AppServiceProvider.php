52129d2a18a4f3a60e7ba5419adb92af3f54b050
Enrique Sanchez <enrique.sanchez@futurite.com>
2026-06-17 16:56:55 -0600

    fresh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
 use Filament\Actions\ViewAction;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
+
         ViewAction::configureUsing(function (ViewAction $action): void {
             $action->modalCancelAction(false);
         });
