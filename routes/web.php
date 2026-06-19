055ab776dae6644a8aef18d6f6d53772d885f845
selene <selene@MacBook-Pro-de-selene.local>
2026-06-19 11:47:24 -0600

    ultimos cambios

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,13 @@ Route::get('/', function () {
 });
 
 Route::get('/diagnostico/{token}', \App\Livewire\ResponderTamizaje::class)->name('tamizaje.publico');
+
+Route::get('/preview-email', function () {
+    $empresa = \App\Models\Empresa::first() ?? new \App\Models\Empresa([
+        'folio' => 'MF-2026-0001',
+        'nombre_empresa' => 'Mi Organización de Prueba S.A.',
+        'nombre_responsable' => 'Juan Pérez',
+        'correo' => 'juan.perez@ejemplo.com',
+    ]);
+    return new \App\Mail\AccesosTableroEmpresa($empresa, 'PasswordTemporal123!');
+});
